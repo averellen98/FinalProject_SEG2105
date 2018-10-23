844ad692954c43c79124b6151a3741d1392a5a20
averellen98 <avere017@uottawa.ca>
2018-10-22 22:38:11 -0400

    Update LogInActivity.java

--- application/app/src/main/java/com/project/group/group_project/LogInActivity.java
+++ application/app/src/main/java/com/project/group/group_project/LogInActivity.java
@@ -5,11 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
 
 public class LogInActivity extends AppCompatActivity {
 
     private TextView email;
     private TextView password;
+    public static List<User> theUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +30,31 @@ public class LogInActivity extends AppCompatActivity {
     public void logInOnClick(View view) {
 
         User user = null;
+        String u_email = email.getText().toString();
+        String u_password = password.getText().toString();
 
-        //code to get user from db using email and password
+        theUsers = SignUpActivity.users;
+        for (int i = 0; i < theUsers.size(); i++){
+            User thisUser = theUsers.get(i);
+            if (thisUser.getEmail() == u_email && thisUser.getPassword() == u_password){
+                //check if this user exists
+                Toast.makeText(this, "User found.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, WelcomeActivity.class);
+                intent.putExtra(MainActivity.USER_FIRSTNAME_KEY, user.getFirstName());
+                intent.putExtra(MainActivity.USER_ROLE_KEY, user.getRole().getName());
 
-        //need to implement message to user if their account was not found instead of going to next activity.
+                startActivity(intent);
+            }
+            if (i == theUsers.size()){
+                //need to implement message to user if their account was not found instead of going to next activity.
+                Toast.makeText(this, "User not found, please try again.", Toast.LENGTH_SHORT).show();
+                email.setText("");
+                password.setText("");
+            }
+        }
 
-        Intent intent = new Intent(this, WelcomeActivity.class);
-        intent.putExtra(MainActivity.USER_FIRSTNAME_KEY, user.getFirstName());
-        intent.putExtra(MainActivity.USER_ROLE_KEY, user.getRole().getName());
 
-        startActivity(intent);
+
+
     }
 }
