896711da7ab9cb8b7d4dbf896058176282cce764
averellen98 <avere017@uottawa.ca>
2018-10-22 22:38:48 -0400

    Update SignUpActivity.java

--- application/app/src/main/java/com/project/group/group_project/SignUpActivity.java
+++ application/app/src/main/java/com/project/group/group_project/SignUpActivity.java
@@ -7,6 +7,16 @@ import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import android.content.Context;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -16,6 +26,8 @@ public class SignUpActivity extends AppCompatActivity {
     private TextView firstName;
     private TextView lastName;
     private RadioGroup roleGroup;
+
+    public static List<User> users = new ArrayList<User>();;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,16 +58,35 @@ public class SignUpActivity extends AppCompatActivity {
 
             User user = createUser(u_email, u_password, role, u_firstName, u_lastName);
 
-            // implement creating user to the database
+            //want to check if the user is already a user, and if not then add it
+            if (users.contains(user)){
+                Toast.makeText(this, "User is already a user, either log in or choose another email.", Toast.LENGTH_LONG).show();
+            }
+            else {
+                // implement creating user to the database
+                DatabaseReference dU = FirebaseDatabase.getInstance().getReference().child("users");
+                String id = dU.push().getKey();
+                String userID = u_email;
 
-            Intent intent = new Intent(this, WelcomeActivity.class);
-            intent.putExtra(MainActivity.USER_ROLE_KEY, role);
-            intent.putExtra(MainActivity.USER_FIRSTNAME_KEY, u_firstName);
+                //the @ and . cause an invalid character so this will fix that
+                userID = id.replace("@", "-");
+                userID = id.replace(".", "-");
 
-            startActivity(intent);
+                User addUser = createUser(userID, u_password, role, u_firstName, u_lastName);
+
+                dU.setValue(addUser);
+                users.add(addUser);
+
+                //start the WelcomeActivity.class
+                Intent intent = new Intent(this, WelcomeActivity.class);
+                intent.putExtra(MainActivity.USER_ROLE_KEY, role);
+                intent.putExtra(MainActivity.USER_FIRSTNAME_KEY, u_firstName);
+
+                startActivity(intent);
+            }
 
         } else {
-            //let the user know they need to correct their input
+            Toast.makeText(this, "Please input all required information.", Toast.LENGTH_LONG).show();
         }
     }
 
