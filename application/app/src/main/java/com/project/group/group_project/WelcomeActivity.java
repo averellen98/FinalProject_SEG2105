9830d153bb6c496c720f6ae28f9a54bbef738e29
averellen98 <avere017@uottawa.ca>
2018-10-24 22:43:47 -0400

    Update WelcomeActivity.java

--- application/app/src/main/java/com/project/group/group_project/WelcomeActivity.java
+++ application/app/src/main/java/com/project/group/group_project/WelcomeActivity.java
@@ -3,9 +3,15 @@ package com.project.group.group_project;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+    String userFirstName;
+    String userRole;
+    Button continueButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +20,14 @@ public class WelcomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
 
         Intent intent = getIntent();
-        String userFirstName = intent.getStringExtra(MainActivity.USER_FIRSTNAME_KEY);
-        String userRole = intent.getStringExtra(MainActivity.USER_ROLE_KEY);
+        userFirstName = intent.getStringExtra(MainActivity.USER_FIRSTNAME_KEY);
+        userRole = intent.getStringExtra(MainActivity.USER_ROLE_KEY);
+        continueButton = (Button) findViewById(R.id.continueButton);
 
         String welcomeMessage = buildWelcomeMessage(userFirstName, userRole);
 
         setWelcomeText(welcomeMessage);
+
     }
 
     private String buildWelcomeMessage(String userFirstName, String userRole) {
@@ -36,5 +44,26 @@ public class WelcomeActivity extends AppCompatActivity {
 
         TextView textView = findViewById(R.id.welcomeMessageText);
         textView.setText(welcomeMessage);
+    }
+
+    public void continueButtonOnClick(View view){
+
+        String role = userRole.toString();
+        Toast.makeText(this, role.toString(),Toast.LENGTH_SHORT).show();
+
+        if (role.toString().equals("Admin")){
+            Intent intent2 = new Intent(this, AdminView.class);
+            startActivity(intent2);
+            setContentView(R.layout.activity_admin_view);
+        } else if (role.toString().equals("Home Owner")){
+            Intent intent3 = new Intent(this, HomeOwnerView.class);
+            startActivity(intent3);
+            setContentView(R.layout.activity_not_implemented);
+        } else if (role.toString().equals("Service Provider")){
+            Intent intent4 = new Intent(this, ServiceProviderView.class);
+            startActivity(intent4);
+            setContentView(R.layout.activity_not_implemented);
+        }
+
     }
 }
