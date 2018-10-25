e55010529259c289ee1e77fcb6081b50e8c45bd6
averellen98 <avere017@uottawa.ca>
2018-10-24 22:47:02 -0400

    Create Services.java

---
+++ application/app/src/main/java/com/project/group/group_project/Services.java
@@ -0,0 +1,16 @@
+package com.project.group.group_project;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Services extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_not_implemented);
+
+
+    }
+}
