cc28164d172dae2f836082adf511e397b2fdc413
averellen98 <avere017@uottawa.ca>
2018-10-24 22:46:18 -0400

    Update LogInActivity.java

--- application/app/src/main/java/com/project/group/group_project/LogInActivity.java
+++ application/app/src/main/java/com/project/group/group_project/LogInActivity.java
@@ -11,12 +11,20 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LogInActivity extends AppCompatActivity {
 
     private TextView email;
     private TextView password;
     public static List<User> theUsers;
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    List<User> products;
+    DataSnapshot snapshot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,34 +35,34 @@ public class LogInActivity extends AppCompatActivity {
         password = findViewById(R.id.passwordText);
     }
 
+
     public void logInOnClick(View view) {
 
-        User user = null;
+        theUsers = SignUpActivity.getUsers();
+        User user;
         String u_email = email.getText().toString();
         String u_password = password.getText().toString();
+        
 
-        theUsers = SignUpActivity.users;
-        for (int i = 0; i < theUsers.size(); i++){
-            User thisUser = theUsers.get(i);
-            if (thisUser.getEmail() == u_email && thisUser.getPassword() == u_password){
-                //check if this user exists
-                Toast.makeText(this, "User found.", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(this, WelcomeActivity.class);
-                intent.putExtra(MainActivity.USER_FIRSTNAME_KEY, user.getFirstName());
-                intent.putExtra(MainActivity.USER_ROLE_KEY, user.getRole().getName());
-
-                startActivity(intent);
-            }
-            if (i == theUsers.size()){
-                //need to implement message to user if their account was not found instead of going to next activity.
-                Toast.makeText(this, "User not found, please try again.", Toast.LENGTH_SHORT).show();
-                email.setText("");
-                password.setText("");
-            }
+        int indexUser = theUsers.indexOf(u_email);
+        if (u_email.equals("admin") && u_password.equals("admin")){
+            Intent intent = new Intent(this, WelcomeActivity.class);
+            startActivity(intent);
         }
-
-
-
+        else if (indexUser != -1){
+            user = theUsers.get(indexUser);
+            //check if this user exists
+            Toast.makeText(this, "User found.", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, WelcomeActivity.class);
+            intent.putExtra(MainActivity.USER_FIRSTNAME_KEY, user.getFirstName());
+            intent.putExtra(MainActivity.USER_ROLE_KEY, user.getRole().getName());
+            startActivity(intent);
+        } else {
+            //need to implement message to user if their account was not found instead of going to next activity.
+            Toast.makeText(this, "User not found, please try again.", Toast.LENGTH_SHORT).show();
+            email.setText("");
+            password.setText("");
+        }
 
     }
 }
