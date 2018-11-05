a0da5a673e636019660d6002b295c65be8494097
averellen98 <avere017@uottawa.ca>
2018-11-05 09:43:13 -0500

    Updated for Unit Testing

--- application/app/src/main/java/com/project/group/group_project/Admin.java
+++ application/app/src/main/java/com/project/group/group_project/Admin.java
@@ -5,4 +5,12 @@ public class Admin extends User {
     public Admin(String id, String username, String password, UserRole role) {
         super(id, username, password, role);
     }
+
+    public boolean canBeAdmin(String id, String username, String password, UserRole role){
+        if (username.equals("admin") && password.equals("admin")){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
