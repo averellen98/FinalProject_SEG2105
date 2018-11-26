d66da28a2b36bb4011c12eea0aec77cf7d9f4420
averellen98 <avere017@uottawa.ca>
2018-11-26 18:34:03 -0500

    Update ServiceTestDatabase.java

--- application/app/src/main/java/com/project/group/group_project/ServiceTestDatabase.java
+++ application/app/src/main/java/com/project/group/group_project/ServiceTestDatabase.java
@@ -1,5 +1,13 @@
 package com.project.group.group_project;
 
+import android.support.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
