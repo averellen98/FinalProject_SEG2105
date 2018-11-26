3abdde73b03681c5d01e69270b7750ce853199eb
averellen98 <avere017@uottawa.ca>
2018-11-26 14:55:06 -0500

    Update HomeOwner.java

--- application/app/src/main/java/com/project/group/group_project/HomeOwner.java
+++ application/app/src/main/java/com/project/group/group_project/HomeOwner.java
@@ -5,4 +5,14 @@ public class HomeOwner extends User {
     public HomeOwner(String id, String username, String password, UserRole role) {
         super(id, username, password, role);
     }
+
+    public static boolean isValidHomeOwner(HomeOwner homeOwner){
+        if (homeOwner.getId() != "" && homeOwner.getUsername() != "" && homeOwner.getPassword() != "" && homeOwner.getFirstName() != "" && homeOwner.getLastName() != ""){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
 }
