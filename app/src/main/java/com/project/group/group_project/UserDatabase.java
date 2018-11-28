1d81568a29582abd7c36c7da0cf9b43d43e0f81b
averellen98 <avere017@uottawa.ca>
2018-11-28 17:32:22 -0500

    Update UserDatabase.java

--- app/src/main/java/com/project/group/group_project/UserDatabase.java
+++ app/src/main/java/com/project/group/group_project/UserDatabase.java
@@ -1,6 +1,7 @@
 package com.project.group.group_project;
 
 import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -11,16 +12,78 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserTestDatabase {
+public class UserDatabase {
+
+    private static final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private static final DatabaseReference databaseUsers = database.getReference("users");
+
+    private static final UserDatabase instance = new UserDatabase();
 
     private static List<User> users = new ArrayList<User>();
 
-    public UserTestDatabase() {
-        addUser("admin", "admin", UserRole.ADMIN, "admin", "admin");
-        addUser("sp", "sp", UserRole.SERVICE_PROVIDER, "sp", "sp");
-        addUser("ho", "ho", UserRole.HOME_OWNER, "ho", "ho");
-        addUser("servicep", "servicep", UserRole.SERVICE_PROVIDER, "service", "p");
-        addUser("homeo", "homeo", UserRole.HOME_OWNER, "home", "o");
+    static {
+
+        databaseUsers.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                users.clear();
+
+                for (DataSnapshot ds: dataSnapshot.getChildren()) {
+
+                    String roleString = ds.child("role").getValue(String.class);
+                    String id = ds.child("id").getValue(String.class);
+                    String username = ds.child("username").getValue(String.class);
+                    String password = ds.child("password").getValue(String.class);
+                    String firstName = ds.child("firstName").getValue(String.class);
+                    String lastName = ds.child("lastName").getValue(String.class);
+
+                    UserRole role = UserRole.getRoleByNameForFirebase(roleString);
+
+                    User user = createUser(id, username, password, role, firstName, lastName);
+
+                    if (role.equals(UserRole.SERVICE_PROVIDER)) {
+
+                        String companyName = ds.child("companyName").getValue(String.class);
+                        String generalDescription = ds.child("generalDescription").getValue(String.class);
+                        int phoneNumber = ds.child("phoneNumber").getValue(Integer.class);
+                        boolean isLicensed = ds.child("isLicensed").getValue(Boolean.class);
+
+                        AddressDatabase addressDatabase = AddressDatabase.getInstance();
+                        Address address = addressDatabase.getAddress(id);
+
+                        AvailabilityDatabase availabilityDatabase = AvailabilityDatabase.getInstance();
+                        List<Availability> availabilities = availabilityDatabase.getAvailabilitiesByServiceProvider(id);
+
+                        ServiceDatabase serviceDatabase = ServiceDatabase.getInstance();
+                        List<Service> services = serviceDatabase.getServiceForProvider(id);
+
+                        ((ServiceProvider) user).setAddress(address);
+                        ((ServiceProvider) user).setAvailabilities(availabilities);
+                        ((ServiceProvider) user).setCompanyName(companyName);
+                        ((ServiceProvider) user).setGeneralDescription(generalDescription);
+                        ((ServiceProvider) user).setLicensed(isLicensed);
+                        ((ServiceProvider) user).setPhoneNumber(phoneNumber);
+                        ((ServiceProvider) user).setServices(services);
+                    }
+
+                    users.add(user);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private UserDatabase() {
+
+    }
+
+    public static UserDatabase getInstance() {
+        return instance;
     }
 
     public boolean isUserAlreadyPresentInDatabase(String username) {
@@ -63,22 +126,29 @@ public class UserTestDatabase {
 
     public User addUser(String username, String password, UserRole role, String firstName, String lastName) {
 
-        String id = "userTestID";
+        String id = databaseUsers.push().getKey();
 
         User user = createUser(id, username, password, role, firstName, lastName);
 
-        users.add(user);
+        databaseUsers.child(id).setValue(user);
 
         return user;
     }
 
     public User addServiceProvider(String username, String password, String firstName, String lastName, String street, String city, String province, String postalCode, int phoneNumber, String companyName, String generalDescription, boolean isLicensed) {
 
-        String id = "spTestID";
+        String id = databaseUsers.push().getKey();
 
         User user = createUser(id, username, password, UserRole.SERVICE_PROVIDER, firstName, lastName);
 
-        users.add(user);
+        databaseUsers.child(id).setValue(user);
+        databaseUsers.child(id).child("phoneNumber").setValue(phoneNumber);
+        databaseUsers.child(id).child("companyName").setValue(companyName);
+        databaseUsers.child(id).child("generalDescription").setValue(generalDescription);
+        databaseUsers.child(id).child("isLicensed").setValue(isLicensed);
+
+        AddressDatabase addressDatabase = AddressDatabase.getInstance();
+        addressDatabase.addAddress(id, street, city, province, postalCode);
 
         return user;
     }
@@ -97,17 +167,6 @@ public class UserTestDatabase {
         }
 
         return null;
-    }
-
-    public boolean canBeAdmin(Admin admin){
-        if (admin.getUsername() != null && admin.getId() != null){
-            for (User user: users){
-                if (user.getRole() == UserRole.ADMIN){
-                    return false;
-                }
-            }
-        }
-        return true;
     }
 
     private static User createUser(String id, String username, String password, UserRole role, String firstName, String lastName) {
@@ -131,6 +190,4 @@ public class UserTestDatabase {
 
         return user;
     }
-
-
 }
